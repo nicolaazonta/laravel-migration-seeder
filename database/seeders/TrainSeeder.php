651f255a119e82c13cda046d2ecc85c30a49585c
nicolaazonta <nicolazonta114@gmail.com>
2023-05-25 18:37:10 +0200

    create trainSeeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +14,21 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+            $train = new Train();
+            $train->company = $faker->randomElement(['trenitalia', 'italo', 'tgv', 'obb']) ;
+            $train->departure_station = $faker->city() ;
+            $train->arrival_station = $faker->city() ;
+            $train->departure_time = $faker->time() ;
+            $train->arrival_time = $faker->time() ;
+            $train->train_code = $faker->numberBetween(00000, 50000) ;
+            $train->total_carriages = $faker->numberBetween(1, 100) ;
+            $train->in_time = $faker->boolean() ;
+            $train->cancelled = $faker->boolean() ;
+
+
+        }
     }
 }
